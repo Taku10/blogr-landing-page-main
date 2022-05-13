8cf9bc3630bd50051056622521e18411e9db96f4
Taku10 <tvmadziwa5@gmail.com>
2022-05-14 00:45:11 +0200

    hi

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const product = document.querySelector(".product");
 const company = document.querySelector(".company");
 const connect = document.querySelector(".connect");
 
-const com_Arrow = document.querySelector(".light-arrow");
-const prod_Arrow = document.querySelector(".light-arrow");
-const connect_Arrow = document.querySelector(".light-arrow");
+const com_Arrow = document.querySelector(".company-wrapper .light-arrow");
+const prod_Arrow = document.querySelector(".product-wrapper .light-arrow");
+const connect_Arrow = document.querySelector(".connect-wrapper .light-arrow");
+
+const com_dark_arrow = document.querySelector(".company-wrapper .dark-arrow");
+const prod_dark_arrow = document.querySelector(".product-wrapper .dark-arrow");
+const connect_dark_arrow = document.querySelector(".connect-wrapper .dark-arrow");
+
 
 
 const productItems = document.querySelector(".product-items");
@@ -16,17 +21,20 @@ const connectItems = document.querySelector(".connect-items")
 const dropProduct = ()=>{
     productItems.classList.toggle("active");
     prod_Arrow.classList.toggle("rotate-arrow");
+    prod_dark_arrow.classList.toggle("rotate-arrow");
 }
 
 const dropCompany = ()=>{
     companyItems.classList.toggle("active");
     com_Arrow.classList.toggle("rotate-arrow");
+    com_dark_arrow.classList.toggle("rotate-arrow");
     
 }
 
 const dropConnect = ()=>{
     connectItems.classList.toggle("active");
     connect_Arrow.classList.toggle("rotate-arrow");
+    connect_dark_arrow.classList.toggle("rotate-arrow");
 }
 
 //event listeners for clicking
