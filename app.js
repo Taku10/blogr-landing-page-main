14a2a4ade082ea550659e2f2e632c6751a134bb7
Taku10 <tvmadziwa5@gmail.com>
2022-05-14 03:01:17 +0200

    init

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const productItems = document.querySelector(".product-items");
 const companyItems = document.querySelector(".company-items");
 const connectItems = document.querySelector(".connect-items")
 
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const nav = document.querySelector(".nav-items");
+
 
 //Drop down function
 const dropProduct = ()=>{
@@ -37,7 +41,36 @@ const dropConnect = ()=>{
     connect_dark_arrow.classList.toggle("rotate-arrow");
 }
 
+//hamburger-menu display and close
+
+const displayMenu = ()=>{ //display menu
+    nav.classList.add("nav-active");
+    hamburger.style.display = "none"
+    close.style.display = "block";
+}
+
+const closeMenu = ()=>{    //close menu
+    nav.classList.remove("nav-active");
+    hamburger.style.display = "block"
+    close.style.display = "none";
+}
+
+
+const menuConfig = ()=>{
+    let width = 768;
+    if(window.innerWidth > width){
+        hamburger.style.display = "none"
+    }else{
+        hamburger.style.display = "block"
+    }
+}
+
+menuConfig();
+
 //event listeners for clicking
 product.addEventListener("click", (dropProduct))
 company.addEventListener("click", (dropCompany))
 connect.addEventListener("click", (dropConnect))
+hamburger.addEventListener("click", (displayMenu))
+close.addEventListener("click", (closeMenu))
+window.addEventListener("resize", (menuConfig))
